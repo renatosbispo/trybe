1961c3cea70d227d27ec3a841caaba406f3f0b6f
Renato Bispo <renatoslvbispo@gmail.com>
2021-12-23 16:16:28 -0300

    Add files for exercise 6 and remove section from exercise 5 file that belonged to exercise 6

--- exercises/back-end/block-19/19.3/exercise-6/dockerized-react-app/src/App.js
+++ exercises/back-end/block-19/19.3/exercise-6/dockerized-react-app/src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          This paragraph was added to test <code>docker-compose up --build -d</code>.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
