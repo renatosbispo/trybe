bd19ac347b66fd825a94a4568b38f41934d4100c
Renato Bispo <renatoslvbispo@gmail.com>
2021-12-27 18:26:10 -0300

    Implement solution for delete practice exercise 4

--- exercises/back-end/block-20/20.4/practice-delete.sql
+++ exercises/back-end/block-20/20.4/practice-delete.sql
@@ -15,3 +15,7 @@ DELETE FROM sakila.actor WHERE first_name = 'MATTHEW';
 -- 3. Delete from sakila.film_text every entry where the description contains
 --    the word 'saga'.
 DELETE FROM sakila.film_text WHERE description LIKE '%saga%';
+
+-- 4. Delete every entry in sakila.film_actor and sakila.film_category.
+TRUNCATE sakila.film_actor;
+TRUNCATE sakila.film_category;
