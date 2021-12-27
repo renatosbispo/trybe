f685318434206db46b1371fdff8b4e8a48c548b1
Renato Bispo <renatoslvbispo@gmail.com>
2021-12-27 16:14:25 -0300

    Implement solution for insert practice exercise 3

--- exercises/back-end/block-20/20.4/practice-insert.sql
+++ exercises/back-end/block-20/20.4/practice-insert.sql
@@ -6,3 +6,7 @@ VALUES ('Elon', 'Musk', 5, 2, 'Elon');
 INSERT INTO sakila.staff (first_name, last_name, address_id, store_id, username) VALUES
 ('Jared', 'Isaacman', 6, 1, 'Jared'),
 ('Hayley', 'Arceneaux', 7, 2, 'Hayley');
+
+-- 3. Select the first five names and last names from sakila.customer and insert them into sakila.actor.
+INSERT INTO sakila.actor (first_name, last_name)
+	SELECT first_name, last_name FROM sakila.customer ORDER BY customer_id ASC LIMIT 5;
