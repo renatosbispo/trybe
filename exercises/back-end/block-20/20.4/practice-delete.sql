dd972f8f20bb40088e44999a80199aee2d03b1c7
Renato Bispo <renatoslvbispo@gmail.com>
2021-12-27 17:55:01 -0300

    Implement solutions for delete practice exercises 1 and 2

---
+++ exercises/back-end/block-20/20.4/practice-delete.sql
@@ -0,0 +1,13 @@
+-- 1. Delete the actor named 'KARL'.
+DELETE FROM sakila.film_actor WHERE actor_id IN (
+	SELECT actor_id FROM sakila.actor WHERE first_name = 'KARL'
+);
+
+DELETE FROM sakila.actor WHERE first_name = 'KARL';
+
+-- 2. Delete the actor named 'MATTHEW'.
+DELETE FROM sakila.film_actor WHERE actor_id IN (
+	SELECT actor_id FROM sakila.actor WHERE first_name = 'MATTHEW'
+);
+
+DELETE FROM sakila.actor WHERE first_name = 'MATTHEW';
