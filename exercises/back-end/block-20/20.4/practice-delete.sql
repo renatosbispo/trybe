4839823f88da50b9c2f639d41b4cf4196cfd5f1b
Renato Bispo <renatoslvbispo@gmail.com>
2021-12-27 17:56:13 -0300

    Fix comment

--- exercises/back-end/block-20/20.4/practice-delete.sql
+++ exercises/back-end/block-20/20.4/practice-delete.sql
@@ -5,7 +5,7 @@ DELETE FROM sakila.film_actor WHERE actor_id IN (
 
 DELETE FROM sakila.actor WHERE first_name = 'KARL';
 
--- 2. Delete the actor named 'MATTHEW'.
+-- 2. Delete the actors named 'MATTHEW'.
 DELETE FROM sakila.film_actor WHERE actor_id IN (
 	SELECT actor_id FROM sakila.actor WHERE first_name = 'MATTHEW'
 );
