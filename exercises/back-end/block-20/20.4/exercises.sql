fb77dc9ac89ca342ab7a8cd6bee456b8b1829bcf
Renato Bispo <renatoslvbispo@gmail.com>
2021-12-27 19:04:13 -0300

    Implement solution for exercise 2

--- exercises/back-end/block-20/20.4/exercises.sql
+++ exercises/back-end/block-20/20.4/exercises.sql
@@ -4,3 +4,9 @@ INSERT INTO Pixar.Movies (title, director, `year`, length_minutes) VALUES
 ('Procurando Nemo', 'John Lasseter', 2003, 107),
 ('Os Incríveis', 'Brad Bird', 2004, 116),
 ('WALL-E', 'Pete Docter', 2008, 104);
+
+-- Exercise 2
+INSERT INTO Pixar.BoxOffice (movie_id, rating, domestic_sales, international_sales)
+SELECT id, 6.8, 450000000, 370000000
+FROM Pixar.Movies
+WHERE title = 'Procurando Nemo';
