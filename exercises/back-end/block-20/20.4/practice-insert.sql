cafdb75139538cddf5c5c3cbeeda74aedaf1171c
Renato Bispo <renatoslvbispo@gmail.com>
2021-12-27 15:54:08 -0300

    Implement solution for insert practice exercise 2

--- exercises/back-end/block-20/20.4/practice-insert.sql
+++ exercises/back-end/block-20/20.4/practice-insert.sql
@@ -1,4 +1,8 @@
--- 1. Add new employee to sakila.staff
+-- 1. Add new employee to sakila.staff.
 INSERT INTO sakila.staff (first_name, last_name, address_id, store_id, username)
 VALUES ('Elon', 'Musk', 5, 2, 'Elon');
 
+-- 2. Insert two new employees to sakila.staff with a single query.
+INSERT INTO sakila.staff (first_name, last_name, address_id, store_id, username) VALUES
+('Jared', 'Isaacman', 6, 1, 'Jared'),
+('Hayley', 'Arceneaux', 7, 2, 'Hayley');
