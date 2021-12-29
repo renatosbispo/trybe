884f83ad3cca99fd8bd02def1c4f4ed4db2ba418
Renato Bispo <renatoslvbispo@gmail.com>
2021-12-29 14:37:00 -0300

    Update comments in math functions practice file with proper exercise names

--- exercises/back-end/block-21/21.1/practice-math-functions.sql
+++ exercises/back-end/block-21/21.1/practice-math-functions.sql
@@ -1,8 +1,8 @@
--- Exercise 1
+-- DIV/MOD challenge 1
 SELECT IF(15 MOD 2 = 0, 'Par', 'Ímpar') AS 'Par ou ímpar';
 
--- Exercise 2
+-- DIV/MOD challenge 2
 SELECT 220 DIV 12;
 
--- Exercise 3
+-- DIV/MOD challenge 3
 SELECT IF(220 MOD 12 > 0, CONCAT('Sim, ', 220 MOD 12), 'Não') AS 'Lugares sobrando';
