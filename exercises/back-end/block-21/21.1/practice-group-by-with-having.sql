9aeab003751da59c1edfbb3c06f2ab29a2b10449
Renato Bispo <renatoslvbispo@gmail.com>
2021-12-29 16:08:54 -0300

    Implement solution for group by with 'having' practice exercise 2

--- exercises/back-end/block-21/21.1/practice-group-by-with-having.sql
+++ exercises/back-end/block-21/21.1/practice-group-by-with-having.sql
@@ -4,3 +4,10 @@ FROM sakila.film
 GROUP BY rating
 HAVING avg_length BETWEEN 115 AND 121.5
 ORDER BY avg_length DESC;
+
+-- Exercise 2
+SELECT rating, SUM(replacement_cost) AS total_replacement_cost
+FROM sakila.film
+GROUP BY rating
+HAVING total_replacement_cost > 3950.5
+ORDER BY total_replacement_cost ASC;
