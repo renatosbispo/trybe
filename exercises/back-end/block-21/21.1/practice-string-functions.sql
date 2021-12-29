320a4016a2f9d343f993c1f90364ed71e1606da0
Renato Bispo <renatoslvbispo@gmail.com>
2021-12-29 14:06:00 -0300

    Implement solution for string functions practice exercise 5

--- exercises/back-end/block-21/21.1/practice-string-functions.sql
+++ exercises/back-end/block-21/21.1/practice-string-functions.sql
@@ -9,3 +9,6 @@ SELECT CHAR_LENGTH('Uma frase qualquer');
 
 -- Exercise 4
 SELECT SUBSTRING('A linguagem JavaScript está entre as mais usadas', 13, 10);
+
+-- Exercise 5
+SELECT LCASE('RUA NORTE 1500, SÃO PAULO, BRASIL');
