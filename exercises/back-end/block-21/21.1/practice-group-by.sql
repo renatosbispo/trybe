9016a6caa8d08d9b6247df18cb051f21a21e464e
Renato Bispo <renatoslvbispo@gmail.com>
2021-12-29 15:45:43 -0300

    Implement solution for group by practice exercise 2

--- exercises/back-end/block-21/21.1/practice-group-by.sql
+++ exercises/back-end/block-21/21.1/practice-group-by.sql
@@ -2,3 +2,9 @@
 SELECT `active`, COUNT(*)
 FROM sakila.customer
 GROUP BY `active`;
+
+-- Exercise 2
+SELECT store_id, `active`, COUNT(*)
+FROM sakila.customer
+GROUP BY store_id, `active`
+ORDER BY store_id ASC;
