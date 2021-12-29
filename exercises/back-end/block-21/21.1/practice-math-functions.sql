f8b836d1f4b877644f1584bbff6f47e0cccb88e1
Renato Bispo <renatoslvbispo@gmail.com>
2021-12-29 14:33:21 -0300

    Implement solution for math functions practice exercise 3

--- exercises/back-end/block-21/21.1/practice-math-functions.sql
+++ exercises/back-end/block-21/21.1/practice-math-functions.sql
@@ -3,3 +3,6 @@ SELECT IF(15 MOD 2 = 0, 'Par', 'Ímpar') AS 'Par ou ímpar';
 
 -- Exercise 2
 SELECT 220 DIV 12;
+
+-- Exercise 3
+SELECT IF(220 MOD 12 > 0, CONCAT('Sim, ', 220 MOD 12), 'Não') AS 'Lugares sobrando';
