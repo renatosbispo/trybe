38b2634cb3ff6e9ead434f6708dee97f8f5afe65
Renato Bispo <renatoslvbispo@gmail.com>
2021-12-29 17:51:49 -0300

    Implement solution for inner join practice exercise 2

--- exercises/back-end/block-21/21.2/practice-inner-join.sql
+++ exercises/back-end/block-21/21.2/practice-inner-join.sql
@@ -10,3 +10,12 @@ SELECT
 FROM sakila.actor
 INNER JOIN sakila.film_actor
 ON sakila.actor.actor_id = sakila.film_actor.actor_id;
+
+-- Exercise 2
+SELECT
+	sakila.staff.first_name,
+    sakila.staff.last_name,
+    sakila.address.address
+FROM sakila.staff
+INNER JOIN sakila.address
+ON sakila.staff.address_id = sakila.address.address_id;
