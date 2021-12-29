b04eef9bfabdf2fdedc0d00d838724632154d69f
Renato Bispo <renatoslvbispo@gmail.com>
2021-12-29 17:05:48 -0300

    Implement solution for exercise 3

--- exercises/back-end/block-21/21.1/exercises.sql
+++ exercises/back-end/block-21/21.1/exercises.sql
@@ -3,3 +3,9 @@ SELECT MAX(SALARY) FROM hr.employees;
 
 -- Exercise 2
 SELECT MAX(SALARY) - MIN(SALARY) FROM hr.employees;
+
+-- Exercise 3
+SELECT JOB_ID, AVG(SALARY)
+FROM hr.employees
+GROUP BY JOB_ID
+ORDER BY AVG(SALARY) DESC;
