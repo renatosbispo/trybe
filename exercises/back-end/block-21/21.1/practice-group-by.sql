560e1ab062fd0f701d7e730d7cc2006fb74d20ba
Renato Bispo <renatoslvbispo@gmail.com>
2021-12-29 15:46:00 -0300

    Implement solution for group by practice exercise 3

--- exercises/back-end/block-21/21.1/practice-group-by.sql
+++ exercises/back-end/block-21/21.1/practice-group-by.sql
@@ -8,3 +8,9 @@ SELECT store_id, `active`, COUNT(*)
 FROM sakila.customer
 GROUP BY store_id, `active`
 ORDER BY store_id ASC;
+
+-- Exercise 3
+SELECT rating, AVG(rental_duration) AS avg_rental_duration
+FROM sakila.film
+GROUP BY rating
+ORDER BY avg_rental_duration DESC;
