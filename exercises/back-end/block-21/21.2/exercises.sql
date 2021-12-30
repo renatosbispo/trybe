9b583ff020a592882f8001eecceb40a923f91d41
Renato Bispo <renatoslvbispo@gmail.com>
2021-12-30 14:25:04 -0300

    Implement solution for exercise 2

--- exercises/back-end/block-21/21.2/exercises.sql
+++ exercises/back-end/block-21/21.2/exercises.sql
@@ -7,3 +7,14 @@ SELECT
 FROM Pixar.Movies
 INNER JOIN Pixar.BoxOffice
 ON Pixar.Movies.id = Pixar.BoxOffice.movie_id;
+
+-- Exercise 2
+SELECT
+	Pixar.Movies.id,
+    Pixar.Movies.title,
+    Pixar.BoxOffice.domestic_sales +
+	Pixar.BoxOffice.international_sales AS total_sales
+FROM Pixar.Movies
+INNER JOIN Pixar.BoxOffice
+ON Pixar.Movies.id = Pixar.BoxOffice.movie_id
+WHERE Pixar.BoxOffice.international_sales > Pixar.BoxOffice.domestic_sales;
