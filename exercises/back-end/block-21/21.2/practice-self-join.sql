5c345021111a98df06b1af358489796ece864a7d
Renato Bispo <renatoslvbispo@gmail.com>
2021-12-30 14:07:44 -0300

    Implement solution for self join practice exercise 2

--- exercises/back-end/block-21/21.2/practice-self-join.sql
+++ exercises/back-end/block-21/21.2/practice-self-join.sql
@@ -8,3 +8,14 @@ FROM hr.employees AS employees_staff
 INNER JOIN hr.employees AS employees_managers
 ON employees_staff.MANAGER_ID = employees_managers.EMPLOYEE_ID
 WHERE employees_staff.DEPARTMENT_ID <> employees_managers.DEPARTMENT_ID;
+
+-- Exercise 2
+SELECT
+	CONCAT(employees_managers.FIRST_NAME, " ", employees_managers.LAST_NAME)
+		AS manager_name,
+	COUNT(*) AS staff_size
+FROM hr.employees AS employees_staff
+INNER JOIN hr.employees AS employees_managers
+ON employees_staff.MANAGER_ID = employees_managers.EMPLOYEE_ID
+GROUP BY manager_name
+ORDER BY staff_size DESC;
