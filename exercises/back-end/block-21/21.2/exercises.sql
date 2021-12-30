28d1ef7e6fb61afff38866298e2f89a2090a0522
Renato Bispo <renatoslvbispo@gmail.com>
2021-12-30 14:34:12 -0300

    Implement solution for exercise 4

--- exercises/back-end/block-21/21.2/exercises.sql
+++ exercises/back-end/block-21/21.2/exercises.sql
@@ -27,3 +27,16 @@ FROM Pixar.Movies
 INNER JOIN Pixar.BoxOffice
 ON Pixar.Movies.id = Pixar.BoxOffice.movie_id
 ORDER BY Pixar.BoxOffice.rating DESC;
+
+-- Exercise 4
+SELECT
+    Pixar.Theater.name,
+    Pixar.Theater.location,
+	Pixar.Movies.title,
+    Pixar.Movies.director,
+    Pixar.Movies.year,
+    Pixar.Movies.length_minutes
+FROM Pixar.Theater
+LEFT JOIN Pixar.Movies
+ON Pixar.Theater.id = Pixar.Movies.theater_id
+ORDER BY Pixar.Theater.name ASC;
