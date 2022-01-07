09de5e04aa73c3c560c6d9bd9a8e53799dca4c13
Renato Bispo <renatoslvbispo@gmail.com>
2022-01-07 15:42:09 -0300

    Implement solution for exercise 3

--- exercises/back-end/chapter-22/subchapter-22.1/my-scripts/imc.js
+++ exercises/back-end/chapter-22/subchapter-22.1/my-scripts/imc.js
@@ -1,5 +1,7 @@
-const weight = 67;
-const height = 1.67;
-const imc = weight / height ** 2;
+const readline = require('readline-sync');
+
+const weight = readline.questionInt('What is your weight? (kg) ');
+const height = readline.questionInt('What is your height? (cm) ')
+const imc = weight / (height / 100) ** 2;
 
 console.log(`Your IMC is ${imc}`);
