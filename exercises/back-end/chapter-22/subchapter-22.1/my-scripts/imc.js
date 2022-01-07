84c0b714066a32f961208fada7a7b5fc5fb3d235
Renato Bispo <renatoslvbispo@gmail.com>
2022-01-07 15:54:42 -0300

    Implement solution for exercise 5

--- exercises/back-end/chapter-22/subchapter-22.1/my-scripts/imc.js
+++ exercises/back-end/chapter-22/subchapter-22.1/my-scripts/imc.js
@@ -4,4 +4,15 @@ const weight = readline.questionFloat('What is your weight? (kg) ');
 const height = readline.questionInt('What is your height? (cm) ')
 const imc = weight / (height / 100) ** 2;
 
-console.log(`Your IMC is ${imc}`);
+let weightCategory = null;
+if (imc < 18.5) {
+  weightCategory = 'Underweight';
+} else if (imc < 25) {
+  weightCategory = 'Healthy Weight';
+} else if (imc < 30) {
+  weightCategory = 'Overweight';
+} else {
+  weightCategory = 'Obesity';
+}
+
+console.log(`\nYour IMC is ${imc.toFixed(2)} (${weightCategory})`);
