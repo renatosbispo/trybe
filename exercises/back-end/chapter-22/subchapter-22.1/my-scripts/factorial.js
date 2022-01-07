8d63f477758361605523555a3b35f133f69029f2
Renato Bispo <renatoslvbispo@gmail.com>
2022-01-07 17:59:54 -0300

    Implement solution for bonus exercise 1

---
+++ exercises/back-end/chapter-22/subchapter-22.1/my-scripts/factorial.js
@@ -0,0 +1,19 @@
+const readline = require('readline-sync');
+
+const getFactorial = (number) => {
+  if (number === 1) return 1;
+
+  return number * getFactorial(number - 1);
+}
+
+let number = 0;
+
+while (number < 1) {
+  number = readline.questionInt('Calculate factorial of: ');
+
+  if (number < 1) {
+    console.log('\nEnter an integer greater than 0.\n');
+  }
+}
+
+console.log(`\nFactorial(${number}) = ${getFactorial(number)}`);
--- exercises/back-end/chapter-22/subchapter-22.1/my-scripts/index.js
+++ exercises/back-end/chapter-22/subchapter-22.1/my-scripts/index.js
@@ -13,6 +13,10 @@ const scripts = [
     name: 'Lottery',
     path: './lottery.js',
   },
+  {
+    name: 'Factorial',
+    path: './factorial.js',
+  },
 ];
 
 console.log('My Scripts');
