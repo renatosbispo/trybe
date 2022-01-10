bbc822d08570ac32779b42603ec40e3ca73609d8
Renato Bispo <renatoslvbispo@gmail.com>
2022-01-10 15:16:56 -0300

    Implement solution for exercise 1

---
+++ exercises/back-end/chapter-22/subchapter-22.2/exercise1.js
@@ -0,0 +1,28 @@
+function exercise1(number1, number2, number3) {
+  return new Promise((resolve, reject) => {
+    if (
+      typeof number1 != 'number' ||
+      typeof number2 != 'number' ||
+      typeof number3 != 'number'
+    ) {
+      reject('Provide only numbers to the function!');
+    }
+
+    const result = (number1 + number2) * number3;
+
+    if (result < 50) {
+      reject('Result was too low (below 50).');
+    }
+
+    resolve(result);
+  });
+}
+
+(async () => {
+  try {
+    const result = await exercise1(7, 3, 5);
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+})();
