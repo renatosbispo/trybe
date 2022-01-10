93054afd22ec09114ae6e7285e16ed304b84926d
Renato Bispo <renatoslvbispo@gmail.com>
2022-01-10 17:25:55 -0300

    Implement solutions for exercises 4.2 to 4.6

--- exercises/back-end/chapter-22/subchapter-22.2/exercise4.js
+++ exercises/back-end/chapter-22/subchapter-22.2/exercise4.js
@@ -2,17 +2,104 @@ const fs = require('fs');
 const { printOnPromiseReject } = require('./utils');
 
 const simpsonsFilePath = './simpsons.json';
+const simpsonFamilyFilePath = './simpsonFamily.json';
+
+async function readFromSimpsonsFile(filePath) {
+  try {
+    const fileContents = await fs.promises.readFile(filePath, 'utf8');
+    const simpsons = JSON.parse(fileContents);
+
+    return simpsons;
+  } catch (error) {
+    printOnPromiseReject(error);
+  }
+}
+
+function writeToSimpsonsFile(simpsons, filePath) {
+  try {
+    fs.writeFileSync(filePath, JSON.stringify(simpsons, null, 2));
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 async function exercise4part1() {
   try {
-    const fileContents = await fs.promises.readFile(simpsonsFilePath, 'utf8');
-    const simpsons = JSON.parse(fileContents);
+    const simpsons = await readFromSimpsonsFile(simpsonsFilePath);
     simpsons.forEach(({ id, name }) => console.log(`${id} - ${name}`));
   } catch (error) {
     printOnPromiseReject(error);
   }
 }
 
+async function exercise4part2(desiredSimpsonId) {
+  return new Promise(async (resolve, reject) => {
+    const simpsons = await readFromSimpsonsFile(simpsonsFilePath);
+    const desiredSimpson = simpsons.find(
+      ({ id }) => desiredSimpsonId === Number(id)
+    );
+
+    if (desiredSimpson) {
+      resolve(desiredSimpson);
+    }
+
+    reject(new Error(`No simpson with id ${desiredSimpsonId} was found.`));
+  });
+}
+
+async function exercise4part3() {
+  const simpsons = await readFromSimpsonsFile(simpsonsFilePath);
+  const updatedSimpsons = simpsons.filter(
+    ({ id }) => id !== '10' && id !== '6'
+  );
+
+  writeToSimpsonsFile(updatedSimpsons, simpsonsFilePath);
+  console.log(await readFromSimpsonsFile(simpsonsFilePath));
+}
+
+async function exercise4part4() {
+  const simpsons = await readFromSimpsonsFile(simpsonsFilePath);
+  const simpsonFamily = simpsons.filter(
+    ({ id }) => Number(id) >= 1 && Number(id) <= 4
+  );
+
+  writeToSimpsonsFile(simpsonFamily, simpsonFamilyFilePath);
+  console.log(await readFromSimpsonsFile(simpsonFamilyFilePath));
+}
+
+async function exercise4part5() {
+  const simpsons = await readFromSimpsonsFile(simpsonsFilePath);
+  const simpsonFamily = await readFromSimpsonsFile(simpsonFamilyFilePath);
+
+  const nelsonMuntz = simpsons.find(({ name }) => name === 'Nelson Muntz');
+  const updatedSimpsonFamily = [...simpsonFamily, nelsonMuntz];
+
+  writeToSimpsonsFile(updatedSimpsonFamily, simpsonFamilyFilePath);
+  console.log(await readFromSimpsonsFile(simpsonFamilyFilePath));
+}
+
+async function exercise4part6() {
+  const simpsons = await readFromSimpsonsFile(simpsonsFilePath);
+  const simpsonFamily = await readFromSimpsonsFile(simpsonFamilyFilePath);
+
+  const maggieSimpson = simpsons.find(({ name }) => name === 'Maggie Simpson');
+  const updatedSimpsonFamily = simpsonFamily.filter(
+    ({ name }) => name !== 'Nelson Muntz'
+  );
+
+  writeToSimpsonsFile(
+    [...updatedSimpsonFamily, maggieSimpson],
+    simpsonFamilyFilePath
+  );
+
+  console.log(await readFromSimpsonsFile(simpsonFamilyFilePath));
+}
+
 module.exports = {
   exercise4part1,
+  exercise4part2,
+  exercise4part3,
+  exercise4part4,
+  exercise4part5,
+  exercise4part6,
 };
--- exercises/back-end/chapter-22/subchapter-22.2/index.js
+++ exercises/back-end/chapter-22/subchapter-22.2/index.js
@@ -1,6 +1,13 @@
 const { exercise1, exercise2, exercise3 } = require('./exercises1to3');
 const { printOnPromiseResolve, printOnPromiseReject } = require('./utils');
-const { exercise4part1 } = require('./exercise4');
+const {
+  exercise4part1,
+  exercise4part2,
+  exercise4part3,
+  exercise4part4,
+  exercise4part5,
+  exercise4part6
+} = require('./exercise4');
 
 function printExerciseHeader(exerciseNumber) {
   const exerciseTitle = `\nExercise ${exerciseNumber}`;
@@ -15,7 +22,6 @@ function printExerciseHeader(exerciseNumber) {
 }
 
 async function runExercise1() {
-  printExerciseHeader(1);
   try {
     const result = await exercise1(7, 3, 5);
     printOnPromiseResolve(result);
@@ -24,15 +30,35 @@ async function runExercise1() {
   }
 }
 
+async function runExercise4part2(desiredSimpsonId) {
+  try {
+    const data = await exercise4part2(desiredSimpsonId);
+    printOnPromiseResolve(data);
+  } catch (error) {
+    printOnPromiseReject(error);
+  }
+}
+
 (async () => {
+  printExerciseHeader('1');
   await runExercise1();
 
-  printExerciseHeader(2);
+  printExerciseHeader('2');
   await exercise2();
 
-  printExerciseHeader(3);
+  printExerciseHeader('3');
   await exercise3();
 
-  printExerciseHeader(4);
+  printExerciseHeader('4.1');
   await exercise4part1();
+  printExerciseHeader('4.2');
+  await runExercise4part2(5);
+  printExerciseHeader('4.3');
+  await exercise4part3();
+  printExerciseHeader('4.4');
+  await exercise4part4();
+  printExerciseHeader('4.5');
+  await exercise4part5();
+  printExerciseHeader('4.6');
+  await exercise4part6();
 })();
