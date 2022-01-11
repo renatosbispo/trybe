165e1a60dd7b3dace12ef224e3b053be6ebe2fcb
Renato Bispo <renatoslvbispo@gmail.com>
2022-01-11 15:26:55 -0300

    Implement solution for exercise 2

--- exercises/back-end/chapter-22/subchapter-22.3/exercises1to3.js
+++ exercises/back-end/chapter-22/subchapter-22.3/exercises1to3.js
@@ -1,5 +1,15 @@
 function exercises1to3(number) {
+  if (number < 0) {
+    return 'negative';
+  }
 
+  if (number === 0) {
+    return 'neutral';
+  }
+
+  if (number > 0) {
+    return 'positive';
+  }
 }
 
 module.exports = {
