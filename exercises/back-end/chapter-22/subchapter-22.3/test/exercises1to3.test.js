5b642318413b1bb262492aef6625f8f1f4ab7b19
Renato Bispo <renatoslvbispo@gmail.com>
2022-01-11 15:26:26 -0300

    Fix solution for exercise 1

--- exercises/back-end/chapter-22/subchapter-22.3/test/exercises1to3.test.js
+++ exercises/back-end/chapter-22/subchapter-22.3/test/exercises1to3.test.js
@@ -3,19 +3,19 @@ const { expect } = require('chai');
 
 describe('Exercise 1', () => {
   it('If the number is greater than 0 it must return "positive"', () => {
-    const answer = exercise1(10);
+    const answer = exercises1to3(10);
 
     expect(answer).to.be.equal('positive');
   });
 
   it('If the number is less than 0 it must return "negative"', () => {
-    const answer = exercise1(-5);
+    const answer = exercises1to3(-5);
 
     expect(answer).to.be.equal('negative');
   });
 
   it('If the number is equal to 0 it must return "neutral"', () => {
-    const answer = exercise1(0);
+    const answer = exercises1to3(0);
 
     expect(answer).to.be.equal('neutral');
   });
