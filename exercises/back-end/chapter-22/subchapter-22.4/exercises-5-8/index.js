1a08f20ae00f694e2b66feafb04817ec35f04b27
Renato Bispo <renatoslvbispo@gmail.com>
2022-01-12 17:37:19 -0300

    Implement solution for exercise 7

--- exercises/back-end/chapter-22/subchapter-22.4/exercises-5-8/index.js
+++ exercises/back-end/chapter-22/subchapter-22.4/exercises-5-8/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 });
-
+console.log(readSimpsons());
 app.get('/simpsons', (_, res) => {
   const { simpsons, error } = readSimpsons();
 
@@ -20,4 +20,28 @@ app.get('/simpsons', (_, res) => {
   }
 
   res.status(200).json(simpsons);
+});
+
+app.get('/simpsons/:id', (req, res) => {
+  const { id: targetId } = req.params;
+
+  const { simpsons, error } = readSimpsons();
+
+  if (error) {
+    res.status(500).send();
+    return;
+  }
+
+  const targetSimpson = simpsons.find(
+    ({ id: simpsonId }) => simpsonId === targetId
+  );
+
+  if (!targetSimpson) {
+    res
+      .status(404)
+      .json({ message: `No character with id ${targetId} was found.` });
+    return;
+  }
+
+  res.status(200).json(targetSimpson);
 });
