db331af8b2e9ab89bcdaf5666d39db227bcb355b
Renato Bispo <renatoslvbispo@gmail.com>
2022-01-13 22:34:45 -0300

    Create automatic route setup mechanism

--- exercises/back-end/chapter-22/subchapter-22.5/exercises/index.js
+++ exercises/back-end/chapter-22/subchapter-22.5/exercises/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const routes = require('./routes');
 
 const app = express();
-const port = 3001;
+const port = 3000;
 
 app.use(bodyParser.json());
+
+routes.forEach(({ route, router }) => app.use(route, router));
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
---
+++ exercises/back-end/chapter-22/subchapter-22.5/exercises/routes/user.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/register');
+
+router.post('/login');
+
+module.exports = {
+  route: '/user',
+  router,
+}
