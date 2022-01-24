2a4e70673ee35c9f5c607dfbc7fa3ad722fa11b3
Renato Bispo <renatosilvabispo@outlook.com>
2022-01-24 17:54:24 -0300

    Add ping endpoint

--- exercises/back-end/chapter-23/index.js
+++ exercises/back-end/chapter-23/index.js
@@ -3,6 +3,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/ping', (_, res) => {
+  res.status(200).json({ message: 'pong!' });
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}...`);
 });
