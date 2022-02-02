5b3a814588ef6d2e49a5e83228b92b3e2f522a52
Renato Bispo <renatosilvabispo@outlook.com>
2022-02-01 22:07:57 -0300

    Create POST /books and PUT /books/:id routes

--- exercises/back-end/chapter-24/index.js
+++ exercises/back-end/chapter-24/index.js
@@ -10,26 +10,61 @@ const handleError = (_error, _req, res, _next) => {
 
 app.use(express.json());
 
-app.get('/books', async (_, res, next) => {
-  try {
-    const books = await book.findAll();
+app
+  .get('/books', async (_, res, next) => {
+    try {
+      const books = await book.findAll();
 
-    res.status(200).json(books);
-  } catch (error) {
-    next(error);
-  }
-});
+      res.status(200).json(books);
+    } catch (error) {
+      next(error);
+    }
+  })
+  .post('/books', async (req, res, next) => {
+    try {
+      const { title, author, pageQuantity } = req.body;
 
-app.get('/books/:id', async (req, res, next) => {
-  try {
-    const targetId = req.params.id;
-    const targetBook = await book.findOne({ where: { id: targetId } });
+      const newBook = await book.create({ title, author, pageQuantity });
 
-    res.status(200).json(targetBook);
-  } catch (error) {
-    next(error);
-  }
-});
+      res.status(201).json(newBook);
+    } catch (error) {
+      next(error);
+    }
+  })
+  .get('/books/:id', async (req, res, next) => {
+    try {
+      const targetId = req.params.id;
+      const targetBook = await book.findOne({ where: { id: targetId } });
+
+      res.status(200).json(targetBook);
+    } catch (error) {
+      next(error);
+    }
+  })
+  .put('/books/:id', async (req, res, next) => {
+    try {
+      const targetId = req.params.id;
+      const { title, author, pageQuantity } = req.body;
+      const {
+        title: currentTitle,
+        author: currentAuthor,
+        pageQuantity: currentPageQuantity,
+      } = await book.findOne({ where: { id: targetId } });
+
+      await book.update(
+        {
+          title: title || currentTitle,
+          author: author || currentAuthor,
+          pageQuantity: pageQuantity || currentPageQuantity,
+        },
+        { where: { id: targetId } }
+      );
+
+      res.status(204).end();
+    } catch (error) {
+      next(error);
+    }
+  });
 
 app.use(handleError);
 
