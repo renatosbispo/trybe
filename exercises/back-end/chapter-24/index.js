8949737c196c33539d0dfd4c5213fce113152505
Renato Bispo <renatosilvabispo@outlook.com>
2022-02-01 21:44:13 -0300

    Create GET /books and GET /books/:id routes

--- exercises/back-end/chapter-24/index.js
+++ exercises/back-end/chapter-24/index.js
@@ -1,1 +1,38 @@
+require('dotenv').config();
 const express = require('express');
+const { book } = require('./models');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const handleError = (_error, _req, res, _next) => {
+  res.status(500).json({ message: 'Something is wrong' });
+};
+
+app.use(express.json());
+
+app.get('/books', async (_, res, next) => {
+  try {
+    const books = await book.findAll();
+
+    res.status(200).json(books);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get('/books/:id', async (req, res, next) => {
+  try {
+    const targetId = req.params.id;
+    const targetBook = await book.findOne({ where: { id: targetId } });
+
+    res.status(200).json(targetBook);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use(handleError);
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
