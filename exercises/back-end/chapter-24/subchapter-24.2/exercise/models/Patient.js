b8e17bc35733d060d7eb45f2cdf3bf7ae9306ccb
Renato Bispo <renatosilvabispo@outlook.com>
2022-03-08 19:34:19 -0300

    Create Patient model

---
+++ exercises/back-end/chapter-24/subchapter-24.2/exercise/models/Patient.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+  const Patient = sequelize.define('Patient', {
+    patientId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    fullname: DataTypes.STRING,
+    planId: {
+      type: DataTypes.INTEGER,
+      foreignKey: true,
+    },
+  });
+
+  Patient.associate = (models) => {
+    Patient.hasOne(models.Plan, {
+      foreignKey: 'plan_id',
+      as: 'patients'
+    })
+  };
+
+  return Patient;
+};
