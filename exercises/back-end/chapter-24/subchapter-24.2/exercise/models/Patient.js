1445406dd1a5c78c66479d23884a433c43089805
Renato Bispo <renatosilvabispo@outlook.com>
2022-03-08 20:00:02 -0300

    Create Surgery model

--- exercises/back-end/chapter-24/subchapter-24.2/exercise/models/Patient.js
+++ exercises/back-end/chapter-24/subchapter-24.2/exercise/models/Patient.js
@@ -1,22 +1,30 @@
 module.exports = (sequelize, DataTypes) => {
-  const Patient = sequelize.define('Patient', {
-    patientId: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
+  const Patient = sequelize.define(
+    'Patient',
+    {
+      patientId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      fullname: DataTypes.STRING,
+      planId: {
+        type: DataTypes.INTEGER,
+        foreignKey: true,
+      },
     },
-    fullname: DataTypes.STRING,
-    planId: {
-      type: DataTypes.INTEGER,
-      foreignKey: true,
-    },
-  });
+    {
+      timestamps: false,
+      tableName: 'Patient',
+      underscored: true,
+    }
+  );
 
   Patient.associate = (models) => {
     Patient.hasOne(models.Plan, {
       foreignKey: 'plan_id',
-      as: 'patients'
-    })
+      as: 'patients',
+    });
   };
 
   return Patient;
