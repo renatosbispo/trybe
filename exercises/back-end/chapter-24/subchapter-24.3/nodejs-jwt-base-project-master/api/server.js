d57aa31cac67b820632a53ba2dc63c0166f0c487
Renato Bispo <renatosilvabispo@outlook.com>
2022-03-09 18:34:21 -0300

    Create and apply jwt validation middleware

--- exercises/back-end/chapter-24/subchapter-24.3/nodejs-jwt-base-project-master/api/server.js
+++ exercises/back-end/chapter-24/subchapter-24.3/nodejs-jwt-base-project-master/api/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
+const validateJwt = require('../api/auth/validateJwt');
+
 
 const PORT = process.env.PORT || 8080;
 
@@ -11,7 +13,7 @@ app.use(bodyParser.json());
 
 const apiRoutes = express.Router();
 
-apiRoutes.get('/api/posts', routes.getPosts);
+apiRoutes.get('/api/posts', validateJwt, routes.getPosts);
 apiRoutes.post('/api/users', routes.createUsers);
 apiRoutes.get('/api/users', routes.getUsers);
 apiRoutes.post('/api/login', routes.login);
--- exercises/back-end/chapter-24/subchapter-24.3/nodejs-jwt-base-project-master/controllers/login.js
+++ exercises/back-end/chapter-24/subchapter-24.3/nodejs-jwt-base-project-master/controllers/login.js
@@ -1,19 +1,34 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const { User } = require('../models');
 
 module.exports = async (req, res) => {
   try {
-  const { username, password } = req.body;
+    const { username, password } = req.body;
 
-  if (!username || !password)
-    return res.status(401).json({ message: 'É necessário usuário e senha para fazer login' });
+    if (!username || !password)
+      return res
+        .status(401)
+        .json({ message: 'É necessário usuário e senha para fazer login' });
 
-  const user = await User.findOne({ where: { username } });
+    const user = await User.findOne({ where: { username } });
 
-  if (!user || user.password !== password)
-    return res.status(401).json({ message: 'Usuário não existe ou senha inválida' });
+    if (!user || user.password !== password)
+      return res
+        .status(401)
+        .json({ message: 'Usuário não existe ou senha inválida' });
 
-  return res.status(200).json({ message: 'Login efetuado com sucesso'});
+    const jwtConfig = {
+      expiresIn: '7d',
+      algorithm: 'HS256',
+    };
+
+    const token = jwt.sign({ data: user }, process.env.SECRET, jwtConfig);
+
+    return res.status(200).json({ token });
   } catch (err) {
-    return res.status(500).json({ message: 'Erro interno', error: err.message });
+    return res
+      .status(500)
+      .json({ message: 'Erro interno', error: err.message });
   }
 };
