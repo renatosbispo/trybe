f1083104c7593221b78e42d35dfaed10bb57b487
Renato Bispo <renatosilvabispo@outlook.com>
2022-03-09 19:07:55 -0300

    Create password validation middleware

---
+++ exercises/back-end/chapter-24/subchapter-24.3/hello-jwt/middlewares/validatePassword.js
@@ -0,0 +1,17 @@
+const Joi = require('joi');
+
+module.exports = (req, _res, next) => {
+  try {
+    const { password } = req.body;
+    const passwordSchema = Joi.string().min(5);
+    const { error } = passwordSchema.validate(password);
+
+    if (error) {
+      throw error;
+    }
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
