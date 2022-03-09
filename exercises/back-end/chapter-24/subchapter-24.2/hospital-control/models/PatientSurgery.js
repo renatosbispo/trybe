a1f5bec904a475cf4bed6dd9f1c762696f82e5f8
Renato Bispo <renatosilvabispo@outlook.com>
2022-03-09 13:22:32 -0300

    Create PatientSurgery model

---
+++ exercises/back-end/chapter-24/subchapter-24.2/hospital-control/models/PatientSurgery.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, _) => {
+  const PatientSurgery = sequelize.define(
+    'PatientSurgery',
+    {},
+    {
+      timestamps: false,
+      tableName: 'Patient_surgeries',
+    }
+  );
+
+  PatientSurgery.associate = (models) => {
+    models.Patient.belongsToMany(models.Surgery, {
+      as: 'surgeries',
+      through: PatientSurgery,
+      foreignKey: 'patient_id',
+      otherKey: 'surgery_id',
+    });
+
+    models.Surgery.belongsToMany(models.Patient, {
+      as: 'patients',
+      through: PatientSurgery,
+      foreignKey: 'surgery_id',
+      otherKey: 'patient_id',
+    });
+  };
+
+  return PatientSurgery;
+};
