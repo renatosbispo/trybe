295d58d1c916fe858f45fa9067e6b372c3a763b6
Renato Bispo <renatosilvabispo@outlook.com>
2022-03-18 10:33:47 -0300

    Format with prettier

--- exercises/back-end/chapter-24/subchapter-24.4/nodejs-jwt-base-project-master-tests/models/User.js
+++ exercises/back-end/chapter-24/subchapter-24.4/nodejs-jwt-base-project-master-tests/models/User.js
@@ -1,13 +1,16 @@
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    username: DataTypes.STRING,
-    password: DataTypes.STRING,
-  },
-  {
-    timestamps: false,
-    tableName: 'Users',
-  });
+  const User = sequelize.define(
+    'User',
+    {
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      username: DataTypes.STRING,
+      password: DataTypes.STRING,
+    },
+    {
+      timestamps: false,
+      tableName: 'Users',
+    }
+  );
 
   return User;
 };
