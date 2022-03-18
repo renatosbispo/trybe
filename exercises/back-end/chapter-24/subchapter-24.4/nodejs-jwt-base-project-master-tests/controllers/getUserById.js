a004acbd7b4310b2bb53780e80678db3ba72b9ff
Renato Bispo <renatosilvabispo@outlook.com>
2022-03-18 15:07:37 -0300

    Implement solution for exercise 2

--- exercises/back-end/chapter-24/subchapter-24.4/nodejs-jwt-base-project-master-tests/controllers/getUserById.js
+++ exercises/back-end/chapter-24/subchapter-24.4/nodejs-jwt-base-project-master-tests/controllers/getUserById.js
@@ -1,1 +1,12 @@
-module.exports = async (req, res) => res.end();
+module.exports = async (req, res) => {
+  try {
+    const { id: tokenUserId } = req.user;
+    const { id: paramsUserId } = req.params;
+
+    if (tokenUserId !== paramsUserId) {
+      throw new Error('Acesso negado');
+    }
+  } catch (error) {
+    res.status(401).json({ message: error.message });
+  }
+};
