4a721b49cdb9b0b01f9c5ff1437b4b5b9ec2b483
Renato Bispo <renatosilvabispo@outlook.com>
2022-03-18 13:58:14 -0300

    Create POST /api/users integration tests

--- exercises/back-end/chapter-24/subchapter-24.4/nodejs-jwt-base-project-master-tests/tests/createUsers.test.js
+++ exercises/back-end/chapter-24/subchapter-24.4/nodejs-jwt-base-project-master-tests/tests/createUsers.test.js
@@ -11,10 +11,12 @@ const { expect } = chai;
 describe('/api/users', () => {
   before(() => {
     sinon.stub(User, 'findAll').callsFake(userMock.findAll);
+    sinon.stub(User, 'create').callsFake(userMock.create);
   });
 
   after(() => {
     User.findAll.restore();
+    User.create.restore();
   });
 
   describe('GET /api/users', () => {
@@ -30,6 +32,58 @@ describe('/api/users', () => {
 
     it('The request must return the status code 200', () => {
       expect(response).to.have.status(200);
+    });
+  });
+
+  describe('POST /api/users', () => {
+    let createRequestResponse = {};
+    let userListBeforeCreate = [];
+    let userListAfterCreate = [];
+
+    const newUser = {
+      username: 'Elon Musk',
+      password: 'boringpassword',
+    };
+
+    before(async () => {
+      userListBeforeCreate = (await chai.request(server).get('/api/users')).body;
+
+      createRequestResponse = await chai
+        .request(server)
+        .post('/api/users')
+        .send(newUser);
+
+      userListAfterCreate = (await chai.request(server).get('/api/users')).body;
+    });
+
+    it('Before user creation, GET /api/users must return a list with two User entries', () => {
+      expect(userListBeforeCreate).to.have.length(2);
+    });
+
+    it('The POST request must return the status code 201', () => {
+      expect(createRequestResponse).to.have.status(201);
+    });
+
+    it('The POST request must return an object in the response body', () => {
+      expect(createRequestResponse.body).to.be.an('object');
+    });
+
+    it('The object must have the property "message"', () => {
+      expect(createRequestResponse.body).to.have.property('message');
+    });
+
+    it('The property "message" must have the value "Novo usuário criado com sucesso"', () => {
+      expect(createRequestResponse.body.message).to.be.equal(
+        'Novo usuário criado com sucesso'
+      );
+    });
+
+    it('After user creation, GET /api/users must return a list with three User entries', () => {
+      expect(userListAfterCreate).to.have.length(3);
+    });
+
+    it('The list must contain the user sent in the POST request', () => {
+      expect(userListAfterCreate[2]).to.contain(newUser);
     });
   });
 });
