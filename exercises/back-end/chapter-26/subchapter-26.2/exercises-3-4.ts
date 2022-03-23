a1c1bc37fa5edacfea2bdb3e84fd977d9e338027
Renato Bispo <renatosilvabispo@outlook.com>
2022-03-23 20:12:40 -0300

    Implement solution for exercise 4

--- exercises/back-end/chapter-26/subchapter-26.2/exercises-3-4.ts
+++ exercises/back-end/chapter-26/subchapter-26.2/exercises-3-4.ts
@@ -1,3 +1,5 @@
+/* Exercise 3 */
+
 type SliceAmount = 4 | 6 | 8;
 
 interface Pizza {
@@ -5,8 +7,8 @@ interface Pizza {
   slices: SliceAmount;
 }
 
-const pepperoniPizza: Pizza = {
-  flavor: 'Pepperoni',
+const calabresaPizza: Pizza = {
+  flavor: 'Calabresa',
   slices: 8,
 };
 
@@ -17,5 +19,53 @@ const margheritaPizza: Pizza = {
 
 const nutellaPizza: Pizza = {
   flavor: 'Nutella',
+  slices: 4,
+};
+
+/* Exercise 4 */
+
+type RegularPizzaFlavor = 'Diavola' | 'Calabresa' | 'Chicken';
+interface RegularPizza extends Pizza {
+  flavor: RegularPizzaFlavor;
+}
+
+const diavolaPizza: RegularPizza = {
+  flavor: 'Diavola',
+  slices: 4,
+};
+
+const anotherCalabresaPizza: RegularPizza = {
+  flavor: 'Calabresa',
+  slices: 6,
+};
+
+const chickenPizza: RegularPizza = {
+  flavor: 'Chicken',
+  slices: 8,
+};
+
+type VegetarianPizzaFlavor = 'Margherita' | 'Heart of palm' | 'Mushroom';
+interface VegetarianPizza extends Pizza {
+  flavor: VegetarianPizzaFlavor;
+}
+
+const anotherMargheritaPizza: VegetarianPizza = {
+  flavor: 'Margherita',
+  slices: 4,
+};
+
+const mushroomPizza: VegetarianPizza = {
+  flavor: 'Mushroom',
+  slices: 6,
+};
+
+type SweetPizzaFlavor = 'Nutella' | 'Guava paste and cheese' | 'Marshmallow';
+interface SweetPizza extends Pizza {
+  flavor: SweetPizzaFlavor;
+  slices: 4;
+}
+
+const marshmallowPizza: SweetPizza = {
+  flavor: 'Marshmallow',
   slices: 4,
 };
