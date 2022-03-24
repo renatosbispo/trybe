61d53afddda99ec4d9c412be6ab246d1be52ed34
Renato Bispo <renatosilvabispo@outlook.com>
2022-03-24 16:10:06 -0300

    Turn method into arrow function to solve "this" issue

--- exercises/back-end/chapter-26/subchapter-26.3/express-typescript/controllers/books.controller.ts
+++ exercises/back-end/chapter-26/subchapter-26.3/express-typescript/controllers/books.controller.ts
@@ -4,8 +4,13 @@ import { StatusCodes } from 'http-status-codes';
 import BookService from '../services/books.service';
 
 class BooksController {
-  bookService = new BookService();
-  public async getAll(_req: Request, res: Response) {
+  public bookService: BookService;
+
+  constructor() {
+    this.bookService = new BookService();
+  }
+
+  public getAll = async (_req: Request, res: Response) => {
     const books = await this.bookService.getAll();
     res.status(StatusCodes.OK).json(books);
   }
