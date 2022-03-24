4ebe6f16752b615517790b32fd99fc066952621f
Renato Bispo <renatosilvabispo@outlook.com>
2022-03-24 17:47:30 -0300

    Add express.json to api

--- exercises/back-end/chapter-26/subchapter-26.3/express-typescript/index.ts
+++ exercises/back-end/chapter-26/subchapter-26.3/express-typescript/index.ts
@@ -2,9 +2,10 @@ import express, { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import BooksRoutes from './routes/books.routes';
 
-const app = express();
-
+const app: express.Application = express();
 const PORT = 8000;
+
+app.use(express.json());
 
 app.get('/', (_req: Request, res: Response) => {
     res.status(StatusCodes.OK).send('Express + TypeScript')
