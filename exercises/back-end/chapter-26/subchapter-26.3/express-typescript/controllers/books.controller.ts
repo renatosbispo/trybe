dca8c820bc8f2102cdb180efb46de176356a601f
Renato Bispo <renatosilvabispo@outlook.com>
2022-03-24 16:30:28 -0300

    Restore original code and fix it

--- exercises/back-end/chapter-26/subchapter-26.3/express-typescript/controllers/books.controller.ts
+++ exercises/back-end/chapter-26/subchapter-26.3/express-typescript/controllers/books.controller.ts
@@ -1,15 +1,12 @@
+// ./controllers/books.controller.ts
+
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
 import BookService from '../services/books.service';
 
 class BooksController {
-  public bookService: BookService;
-
-  constructor() {
-    this.bookService = new BookService();
-  }
-
+  bookService = new BookService();
   public getAll = async (_req: Request, res: Response) => {
     const books = await this.bookService.getAll();
     res.status(StatusCodes.OK).json(books);
