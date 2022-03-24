a7fe01f713a1cb614b4b32d5a685ae104227bb7d
Renato Bispo <renatosilvabispo@outlook.com>
2022-03-24 15:37:39 -0300

    Create book route file

--- exercises/back-end/chapter-26/subchapter-26.3/express-typescript/index.ts
+++ exercises/back-end/chapter-26/subchapter-26.3/express-typescript/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import BooksRoutes from './routes/books.routes';
 
 const app = express();
 
@@ -8,6 +9,8 @@ const PORT = 8000;
 app.get('/', (_req: Request, res: Response) => {
     res.status(StatusCodes.OK).send('Express + TypeScript')
 });
+
+app.use(BooksRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
