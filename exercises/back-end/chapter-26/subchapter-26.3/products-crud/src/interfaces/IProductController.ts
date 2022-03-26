25ed0ccbc0585dd7e695f53bd3ada11d6146b949
Renato Bispo <renatosilvabispo@outlook.com>
2022-03-26 18:18:11 -0300

    Fix generics

--- exercises/back-end/chapter-26/subchapter-26.3/products-crud/src/interfaces/IProductController.ts
+++ exercises/back-end/chapter-26/subchapter-26.3/products-crud/src/interfaces/IProductController.ts
@@ -2,11 +2,16 @@ import { RequestHandler } from 'express';
 import IProduct from './IProduct';
 
 export default interface IProductController {
-  create: RequestHandler<any, IProduct>;
+  create: RequestHandler<any, IProduct, IProduct>;
   delete: RequestHandler<{ id: string }>;
-  getAll: RequestHandler;
-  getById: RequestHandler<{ id: string }>;
-  getInPriceRange: RequestHandler<{ minPrice: string, maxPrice: string }>;
-  getNotExpried: RequestHandler;
-  update: RequestHandler<{ id: string }, IProduct>;
+  getAll: RequestHandler<any, IProduct[]>;
+  getById: RequestHandler<{ id: string }, IProduct>;
+  getInPriceRange: RequestHandler<
+    any,
+    IProduct[],
+    any,
+    { minPrice: string; maxPrice: string }
+  >;
+  getNotExpired: RequestHandler<any, IProduct[]>;
+  update: RequestHandler<{ id: string }, IProduct, IProduct>;
 }
