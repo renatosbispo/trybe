b5df4a1f5a4ce1aa712f2b18a5a7dbc28843fa01
Renato Bispo <renatosilvabispo@outlook.com>
2022-03-26 18:18:35 -0300

    Add create method to product controller

--- exercises/back-end/chapter-26/subchapter-26.3/products-crud/src/controllers/product.ts
+++ exercises/back-end/chapter-26/subchapter-26.3/products-crud/src/controllers/product.ts
@@ -1,8 +1,34 @@
+import { Request, Response, NextFunction } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import { IProduct } from '../interfaces';
 import { ProductService } from '../services';
 
 export default class ProductController {
-  public async exampleMethod(_req: any, res: any) {
-    res.status(StatusCodes.NOT_IMPLEMENTED).end();
+  private service;
+
+  constructor() {
+    this.service = new ProductService();
+  }
+
+  public async create(
+    req: Request<any, IProduct, IProduct>,
+    res: Response<IProduct>,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const { name, brand, price, productionDate, expirationDate } = req.body;
+
+      const newProduct = await this.service.create({
+        name,
+        brand,
+        price,
+        productionDate,
+        expirationDate,
+      });
+
+      res.status(StatusCodes.CREATED).json(newProduct);
+    } catch (error) {
+      next(error);
+    }
   }
 }
