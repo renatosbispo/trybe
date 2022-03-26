d30ef8c4a797e7d58b844e8ae0b25d1378642266
Renato Bispo <renatosilvabispo@outlook.com>
2022-03-26 17:38:22 -0300

    Remove reference to IProductJson

--- exercises/back-end/chapter-26/subchapter-26.3/products-crud/src/interfaces/index.ts
+++ exercises/back-end/chapter-26/subchapter-26.3/products-crud/src/interfaces/index.ts
@@ -1,6 +1,5 @@
 import IProduct from './IProduct';
 import IProductController from './IProductController';
-import IProductJson from './IProductJson';
 import IProductModel from './IProductModel';
 import IProductService from './IProductService';
 
