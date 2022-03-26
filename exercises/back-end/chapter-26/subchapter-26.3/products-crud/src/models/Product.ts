d42cfcf3d37ab9b2da0cd9fb380ccf86dc95a4f7
Renato Bispo <renatosilvabispo@outlook.com>
2022-03-26 15:44:47 -0300

    Add create method to product model

--- exercises/back-end/chapter-26/subchapter-26.3/products-crud/src/models/Product.ts
+++ exercises/back-end/chapter-26/subchapter-26.3/products-crud/src/models/Product.ts
@@ -1,7 +1,30 @@
-import { Pool } from 'mysql2/promise';
+import { Pool, RowDataPacket, ResultSetHeader } from 'mysql2/promise';
+import { IProduct, IProductModel } from '../interfaces';
 
-export default class ProductModel {
-  constructor(connection: Pool) {}
+export default class ProductModel implements IProductModel {
+  private connection;
 
-  public exampleMethod() {}
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+
+  public async create(data: IProduct) {
+    const { name, brand, price, productionDate, expirationDate } = data;
+
+    const [{ insertId }] = await this.connection.execute<ResultSetHeader>(
+      'INSERT INTO products_api.products (brand, price, production_date, expiration_date) VALUES (?, ?, ?, ?, ?)',
+      [name, brand, price, productionDate, expirationDate]
+    );
+
+    const newProduct = {
+      id: insertId,
+      name,
+      brand,
+      price,
+      productionDate,
+      expirationDate,
+    };
+
+    return newProduct;
+  }
 }
