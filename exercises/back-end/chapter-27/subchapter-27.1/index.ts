de7576c79291cbe88121425ea0ef67cdc15ba2d6
Renato Bispo <renatosilvabispo@outlook.com>
2022-04-04 16:57:58 -0300

    Implement solution for exercise 4

--- exercises/back-end/chapter-27/subchapter-27.1/index.ts
+++ exercises/back-end/chapter-27/subchapter-27.1/index.ts
@@ -1,7 +1,30 @@
+import ExerciseHeader from './ExerciseHeader';
+import Customer from './restaurant/Customer';
+import Order from './restaurant/Order';
+import OrderItem from './restaurant/OrderItem';
+import PaymentOption from './restaurant/PaymentOption';
 import Student from './Student';
+
+// Exercises 1 - 2
+new ExerciseHeader('Exercises 1 - 2');
 
 const renatoBispo = new Student(1, 'Renato Bispo', [9.5, 7, 10, 8], [8, 9]);
 
 console.log(renatoBispo);
-console.log('exams avg:', renatoBispo.getExamsGradesAverage());
-console.log('projects avg:', renatoBispo.getProjectsGradesAverage());
+console.log('Exams avg.:', renatoBispo.getExamsGradesAverage());
+console.log('Projects avg.:', renatoBispo.getProjectsGradesAverage());
+
+// Exercises 3 - 4
+new ExerciseHeader('Exercises 3 - 4');
+
+const customer = new Customer(1, 'Renato Bispo');
+
+const orderItems = [
+  new OrderItem('French Fries', 5.99, 2),
+  new OrderItem('Original Coke', 2.99, 2),
+];
+
+const myOrder = new Order(customer.id, orderItems, PaymentOption.Cash, 0.25);
+
+console.log('Order:', myOrder);
+console.log('Order total with discount:', myOrder.getOrderTotalWithDiscount());
--- exercises/back-end/chapter-27/subchapter-27.1/restaurant/Order.ts
+++ exercises/back-end/chapter-27/subchapter-27.1/restaurant/Order.ts
@@ -5,6 +5,18 @@ export default class Order {
   constructor(
     public customerId: number,
     public items: OrderItem[],
-    public paymentOption: PaymentOption
+    public paymentOption: PaymentOption,
+    public discount: number,
   ) {}
+
+  private getOrderTotal() {
+    return this.items.reduce(
+      (total, { price, quantity }) => total + quantity * price,
+      0
+    );
+  }
+
+  public getOrderTotalWithDiscount() {
+    return this.getOrderTotal() * (1 - this.discount);
+  }
 }
