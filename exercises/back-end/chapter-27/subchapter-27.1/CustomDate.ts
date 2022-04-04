cee042f9b436e70504bf25863bdc508d6b546d56
Renato Bispo <renatosilvabispo@outlook.com>
2022-04-04 20:05:36 -0300

    Implement solution for exercise 6

--- exercises/back-end/chapter-27/subchapter-27.1/CustomDate.ts
+++ exercises/back-end/chapter-27/subchapter-27.1/CustomDate.ts
@@ -1,16 +1,68 @@
 export default class CustomDate {
   constructor(public day: number, public month: number, public year: number) {
-    // if (!this.isValidDay || !this.isValidMonth || !this.isValidYear) {
-    //   this.day = 1;
-    //   this.month = 1;
-    //   this.year = 1900;
-    // }
+    if (!this.isValidDate()) {
+      this.day = 1;
+      this.month = 1;
+      this.year = 1900;
+    }
   }
 
-  public isLeapYear(candidateYear: number) {
+  public compare(date: CustomDate) {
+    const yearDiff = this.year - date.year;
+
+    if (yearDiff !== 0) {
+      return yearDiff / Math.abs(yearDiff);
+    }
+
+    const monthDiff = this.month - date.month;
+
+    if (monthDiff !== 0) {
+      return monthDiff / Math.abs(monthDiff);
+    }
+
+    const dayDiff = this.day - date.day;
+
+    if (monthDiff !== 0) {
+      return dayDiff / Math.abs(dayDiff);
+    }
+
+    return 0;
+  }
+
+  public format(pattern: string) {
+    const formattedDate = pattern
+      .replace('aaaa', String(this.year))
+      .replace('aa', String(this.year).slice(2))
+      .replace('mm', `0${String(this.month)}`.slice(-2))
+      .replace('M', this.getMonthName())
+      .replace('dd', `0${String(this.day)}`.slice(-2));
+
+    return formattedDate;
+  }
+
+  public getMonthName() {
+    const monthsNames = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
+
+    return monthsNames[this.month - 1];
+  }
+
+  public isLeapYear() {
     if (
-      candidateYear % 400 === 0 ||
-      (candidateYear % 4 === 0 && candidateYear % 100 !== 0)
+      this.year % 400 === 0 ||
+      (this.year % 4 === 0 && this.year % 100 !== 0)
     ) {
       return true;
     }
@@ -18,10 +70,14 @@ export default class CustomDate {
     return false;
   }
 
+  private isValidDate() {
+    return this.isValidDay() && this.isValidMonth() && this.isValidYear();
+  }
+
   private isValidDay() {
     const monthsLengths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-    if (this.isLeapYear(this.year)) {
+    if (this.isLeapYear()) {
       monthsLengths[1] = 29;
     }
 
