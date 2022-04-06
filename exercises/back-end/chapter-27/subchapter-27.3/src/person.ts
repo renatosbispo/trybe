637f6397baf61c87636bb12e3a9c0230760bdbc4
Renato Bispo <renatosilvabispo@outlook.com>
2022-04-06 18:51:28 -0300

    Refactor person to be an abstract class

---
+++ exercises/back-end/chapter-27/subchapter-27.3/src/person.ts
@@ -0,0 +1,57 @@
+export default abstract class Person {
+  public readonly age: number;
+  protected maxAge: number = 120;
+  protected minNameLength: number = 3;
+
+  constructor(protected _name: string, protected _birthDate: Date) {
+    this.age = this.calculateAge();
+
+    if (!this.isValidBirthDate()) {
+      this.handleInvalidBirthDate();
+    }
+
+    if (!this.isValidName()) {
+      this.handleInvalidName();
+    }
+  }
+
+  protected calculateAge(): number {
+    const timeDiff = Math.abs(Date.now() - new Date(this._birthDate).getTime());
+
+    return Number((timeDiff / (1000 * 3600 * 24) / 365.25).toFixed(1));
+  }
+
+  protected handleInvalidBirthDate(): void {
+    throw new Error(
+      `Birth date cannot be a future date or result in an age greater than ${this.maxAge}`
+    );
+  }
+
+  protected handleInvalidName(): void {
+    throw new Error(
+      `Name must have at least ${this.minNameLength} characters.`
+    );
+  }
+
+  protected isValidBirthDate(): boolean {
+    return Date.now() - this._birthDate.getTime() > 0 && this.age <= this.maxAge;
+  }
+
+  protected isValidName(name: string = this._name): boolean {
+    return name.length >= this.minNameLength;
+  }
+
+  public abstract printInfo(lineEnd: string): void
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(newName: string) {
+    if (!this.isValidName(newName)) {
+      this.handleInvalidName();
+    }
+
+    this._name = newName;
+  }
+}
