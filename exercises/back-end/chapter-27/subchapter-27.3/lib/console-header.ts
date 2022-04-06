c691121c9e056e5558a96993b9d7f99dd7f4d9f4
Renato Bispo <renatosilvabispo@outlook.com>
2022-04-06 18:22:16 -0300

    Improve console header lib

--- exercises/back-end/chapter-27/subchapter-27.3/lib/console-header.ts
+++ exercises/back-end/chapter-27/subchapter-27.3/lib/console-header.ts
@@ -1,20 +1,59 @@
-export default class ConsoleHeader {
-  constructor(protected header: string, protected separatorChar: string = '=') { }
+enum DefaultSeparatorChar {
+  H1 = '=',
+  H2 = '-',
+  H3 = '.',
+}
 
-  protected buildSeparator() {
+export default class ConsoleHeader {
+  public static readonly DefaultSeparatorChar = DefaultSeparatorChar;
+
+  constructor(
+    protected h1SeparatorChar: string = ConsoleHeader.DefaultSeparatorChar.H1,
+    protected h2SeparatorChar: string = ConsoleHeader.DefaultSeparatorChar.H2,
+    protected h3SeparatorChar: string = ConsoleHeader.DefaultSeparatorChar.H3
+  ) {}
+
+  protected static buildSeparator(headerLength: number, separatorChar: string) {
     let separator = '';
 
-    for (let index = 0; index < this.header.length; index += 1) {
-      separator += this.separatorChar;
+    const parsedSeparatorChar =
+      separatorChar.length > 1 ? separatorChar[0] : separatorChar;
+
+    for (let index = 0; index < headerLength; index += 1) {
+      separator += parsedSeparatorChar;
     }
 
     return separator;
   }
 
-  protected printHeader() {
-    const separator = this.buildSeparator();
+  protected static printHeader(header: string, separatorChar: string) {
+    const separator = this.buildSeparator(header.length, separatorChar);
 
-    console.log(`\n${this.header}`);
+    console.log(`\n${header}`);
     console.log(`${separator}\n`);
+  }
+
+  public static H1(header: string): void {
+    this.printHeader(header, this.DefaultSeparatorChar.H1);
+  }
+
+  public H1(header: string): void {
+    ConsoleHeader.printHeader(header, this.h1SeparatorChar);
+  }
+
+  public static H2(header: string): void {
+    this.printHeader(header, this.DefaultSeparatorChar.H2);
+  }
+
+  public H2(header: string): void {
+    ConsoleHeader.printHeader(header, this.h2SeparatorChar);
+  }
+
+  public static H3(header: string): void {
+    this.printHeader(header, this.DefaultSeparatorChar.H3);
+  }
+
+  public H3(header: string): void {
+    ConsoleHeader.printHeader(header, this.h3SeparatorChar);
   }
 }
